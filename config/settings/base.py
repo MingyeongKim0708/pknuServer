55339dba56f602d52a9e663f9a72306c50aff0d6
KimMG <kimmg0708@naver.com>
2022-01-13 15:48:53 +0900

    settings 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +25,7 @@ SECRET_KEY = '^f$vmfqupigqar*bz-26c*=n&00%0j9=g8$hvtjr)82ng)euhk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['3.36.215.208']
+# ALLOWED_HOSTS = ['3.36.215.208']
 
 
 # Application definition
